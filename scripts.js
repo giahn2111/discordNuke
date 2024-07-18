63dabcc83aac697a3c0d6e6a89e41c423e43ef26
PurpleBeastPage <151061010+PurpleBeastPage@users.noreply.github.com>
2024-07-18 11:20:09 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const backgroundImageUrl = 'https://i.giphy.com/DoHGKMzgXl56jRQfvX.webp'; // Replace with your image URL
+    const backgroundElement = document.querySelector('.background');
+    backgroundElement.style.backgroundImage = `url(${backgroundImageUrl})`;
+});
